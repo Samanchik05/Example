209e8bd715c3b1bb93dbaf60533aa0576586c4c3
Toshev Samandar <toshev1227@gmail.com>
2022-10-25 14:28:27 +0500

    SuperIntizom

---
+++ email/index.js
@@ -0,0 +1,23 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    number: document.getElementById("number").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_xg5jvfa";
+  const templateID = "template_1j3fwvz";
+
+  emailjs.send(serviceID, templateID, params)
+    .then(res => {
+      document.getElementById("name").value = "";
+      document.getElementById("number").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("Xabaringiz muvaffaqiyatli yuborildi!!")
+
+    })
+    .catch(err => console.log(err));
+
+}
+
